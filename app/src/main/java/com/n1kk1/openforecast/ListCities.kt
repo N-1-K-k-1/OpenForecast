e6e72b5dc3cd808a6a2b615442a043d90703c198
N-1-K-k-1 <N1Kk1.pro.123@gmail.com>
2020-10-28 11:51:38 +0300

    initial

---
+++ app/src/main/java/com/n1kk1/openforecast/ListCities.kt
@@ -0,0 +1,11 @@
+package com.n1kk1.openforecast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListCities : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_cities)
+    }
+}
---
+++ gradlew.bat
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="AndroidLayouts">
+    <shared>
+      <config />
+    </shared>
+  </component>
+  <component name="AndroidLogFilters">
+    <option name="TOOL_WINDOW_CONFIGURED_FILTER" value="Show only selected application" />
+  </component>
+  <component name="ChangeListManager">
+    <list default="true" id="7f2a0927-f7aa-457f-a5a8-4eeb9aa6fa97" name="Default Changelist" comment="">
+      <change beforePath="$PROJECT_DIR$/app/src/main/java/com/n1Kk1/crazybirdy/GameBackground.kt" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/java/com/n1Kk1/crazybirdy/GameBackground.kt" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/java/com/n1Kk1/crazybirdy/MainGameView.kt" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/java/com/n1Kk1/crazybirdy/MainGameView.kt" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/res/drawable/background1.png" beforeDir="false" />
+    </list>
+    <option name="SHOW_DIALOG" value="false" />
+    <option name="HIGHLIGHT_CONFLICTS" value="true" />
+    <option name="HIGHLIGHT_NON_ACTIVE_CHANGELIST" value="false" />
+    <option name="LAST_RESOLUTION" value="IGNORE" />
+  </component>
+  <component name="ExecutionTargetManager" SELECTED_TARGET="Pixel_3a_API_30" />
+  <component name="ExternalProjectsData">
+    <projectState path="$PROJECT_DIR$">
+      <ProjectState />
+    </projectState>
+  </component>
+  <component name="ExternalProjectsManager">
+    <system id="GRADLE">
+      <state>
+        <projects_view />
+      </state>
+    </system>
+  </component>
+  <component name="FileTemplateManagerImpl">
+    <option name="RECENT_TEMPLATES">
+      <list>
+        <option value="valueResourceFile" />
+        <option value="Kotlin Class" />
+      </list>
+    </option>
+  </component>
+  <component name="Git.Settings">
+    <option name="RECENT_GIT_ROOT_PATH" value="$PROJECT_DIR$" />
+  </component>
+  <component name="ProjectId" id="1iaeJ8BlauqaaYn0UO7d9ZkFM8b" />
+  <component name="ProjectViewState">
+    <option name="hideEmptyMiddlePackages" value="true" />
+    <option name="showExcludedFiles" value="true" />
+    <option name="showLibraryContents" value="true" />
+  </component>
+  <component name="Properties
